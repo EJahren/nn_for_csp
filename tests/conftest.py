34dd28e04d81ec28c5610ae79b2bc7e31d0bd848
Eivind Jahren <eivind.jahren@gmail.com>
2022-12-29 21:15:18 +0100

    Added parser of the txt format

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import subprocess
+from os import scandir
+from os.path import dirname, join
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_dir():
+    return join(dirname(dirname(__file__)), "data")
+
+
+@pytest.fixture(autouse=True, scope="session")
+def fetch_dataset(data_dir):
+    subprocess.call(join(data_dir, "get.sh"), cwd=data_dir)
+
+
+@pytest.fixture(scope="session")
+def big_data_set_files(data_dir):
+    return list(scandir(join(data_dir, "ProblemDataSet200to400")))
